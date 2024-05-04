52b6686fb07580f1f380bc5b65210501cb7a8d31
Julian Marin <jmtexas19@gmail.com>
2024-05-03 23:53:06 -0500

    Change default values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,12 @@
 variable "docker_image" {
   description = "The full URI of the Docker image to deploy"
+  default     = ""
   type        = string
 }
 
 variable "gcp_project" {
   description = "The GCP project ID"
+  default     = "simple-web-app-422301"
   type        = string
 }
 
