9a51f106e592ba21404d08457660dc6e97d2bf00
Julian Marin <jmtexas19@gmail.com>
2024-05-03 23:52:26 -0500

    Install and configure eslint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
