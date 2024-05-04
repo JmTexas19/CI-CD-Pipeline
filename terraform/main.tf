9ba23436d9c01aa249f5b06d9e41e9edb5efb2bd
Julian Marin <jmtexas19@gmail.com>
2024-05-03 23:53:00 -0500

    Make resource public

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,13 @@ resource "google_cloud_run_service" "default" {
   autogenerate_revision_name = true
 }
 
+resource "google_cloud_run_service_iam_member" "invoker" {
+  service  = simple-web-app-service
+  location = var.gcp_region
+  role     = "roles/run.invoker"
+  member   = "allUsers" # This special identifier represents anyone on the internet.
+}
+
 output "service_url" {
   value = google_cloud_run_service.default.status[0].url
 }
